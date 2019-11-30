3ed796bbe505c6c5f34de6fe795b8bf07fad413f
nicksherron <nsherron90@gmail.com>
2019-11-29 21:37:47 -0500

    added new cli options

--- main.go
+++ main.go
@@ -25,11 +25,40 @@ var (
 )
 
 func main() {
+
+	cli.AppHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+   {{if len .Authors}}
+AUTHOR:
+   {{range .Authors}}{{ . }}{{end}}
+   {{end}}
+{{if .VisibleFlags}}
+OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}{{if .Copyright }}
+COPYRIGHT:
+   {{.Copyright}}
+   {{end}}{{if .Version}}
+VERSION:
+   {{.Version}}
+   {{end}}
+`
+
 	app := &cli.App{
+		Authors: []*cli.Author{
+			&cli.Author{
+				Name:  "Nick Sherron",
+				Email: "nsherron90@gmail.com",
+			},
+		},
+		Name:      "proxytuils",
+		HelpName:  "proxytuils",
+		Usage:     "Find and test proxies from the web.",
+		UsageText: "Find and test proxies from the web.",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:        "file",
-				Aliases: []string{"f"},
+				Aliases:     []string{"f"},
 				Value:       "",
 				Usage:       "File name to write to instead of stdout.",
 				Destination: &outFile,
@@ -42,73 +71,76 @@ func main() {
 			},
 			&cli.Uint64Flag{
 				Name:        "limit",
-				Aliases: []string{"l"},
+				Aliases:     []string{"l"},
 				Value:       0,
 				Usage:       "Limit number of good proxies to check before completing.",
 				Destination: &limit,
 			},
 			&cli.DurationFlag{
 				Name:        "timeout",
-				Aliases: []string{"t"},
-				Value:       15*time.Second,
+				Aliases:     []string{"t"},
+				Value:       15 * time.Second,
 				Usage:       "Specify request time out for checking proxies.",
 				Destination: &timeout,
 			},
 			&cli.IntFlag{
 				Name:        "workers",
-				Aliases: []string{"w"},
-				Value:       5,
-				Usage:       "Number of concurrent requests to make for checking proxies.",
+				Aliases:     []string{"w"},
+				Value:       20,
+				Usage:       "Number of (goroutines) concurrent requests to make for checking proxies.",
 				Destination: &workers,
 			},
 			&cli.StringFlag{
 				Name:        "url",
-				Aliases: []string{"u"},
+				Aliases:     []string{"u"},
 				Value:       "https://httpbin.org/ip",
 				Usage:       "The url to test proxies against.",
 				Destination: &testUrl,
 			},
 		},
+		Action: func(c *cli.Context) error {
+			_, _ = fmt.Fprintln(os.Stderr, "Finding proxies ...")
+			proxies := downloadProxies()
+			if !noCheck {
+				checkInit(proxies)
+				if len(good) == 0 {
+					_, _ = fmt.Fprintln(os.Stderr, "no good proxies found")
+					return nil
+				}
+			}
+
+			if !noCheck {
+				if outFile != "" {
+					g, err := os.Create(outFile)
+					check(err)
+					defer g.Close()
+
+					for _, v := range good {
+						fmt.Fprintln(g, v)
+					}
+				} else {
+					for _, v := range good {
+						fmt.Println(v)
+					}
+				}
+			} else {
+				if outFile != "" {
+					g, err := os.Create(outFile)
+					check(err)
+					defer g.Close()
+					for _, v := range proxies {
+						fmt.Fprintln(g, v)
+					}
+				} else {
+					for _, v := range proxies {
+						fmt.Println(v)
+					}
+				}
+			}
+			return nil
+		},
 	}
 
-	_, _ = fmt.Fprintln(os.Stderr, "Finding proxies ...")
-	proxies := downloadProxies()
-	if !noCheck {
-		checkInit(proxies)
-		if len(good) == 0 {
-			_, _ = fmt.Fprintln(os.Stderr, "no good proxies found")
-			return
-		}
-	}
-
-	if !noCheck {
-		if outFile != "" {
-			g, err := os.Create(outFile)
-			check(err)
-			defer g.Close()
-
-			for _, v := range good {
-				fmt.Fprintln(g, v)
-			}
-		} else {
-			for _, v := range good {
-				fmt.Println(v)
-			}
-		}
-	} else {
-		if outFile != "" {
-			g, err := os.Create(outFile)
-			check(err)
-			defer g.Close()
-			for _, v := range proxies {
-				fmt.Fprintln(g, v)
-			}
-		} else {
-			for _, v := range proxies {
-				fmt.Println(v)
-			}
-		}
-	}
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
